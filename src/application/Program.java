ae7abb8e1d891978f296fd1bcbf64a035817b108
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-02-13 11:45:51 +0000

    insert row into coursejbdc

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -12,35 +15,41 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("mm/MM/yyyy");
+		
 		Connection conn = null;
 		
-		Statement st = null;
+		PreparedStatement st = null;
 		
-		ResultSet rs = null;
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement(); // Instance Statement
-			rs = st.executeQuery("select * from department"); // Query
+			st = conn.prepareStatement(
+					"insert into seller "
+					+"(Name,Email, BirthDate,BaseSalary,DepartmentId) "
+					+"values "
+					+"(?, ?, ?, ?, ?)");
+					
+			st.setString(1, "Thiago Rabaioli");
+			st.setString(2, "tfrabaioli@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/09/1985").getTime()));
+			st.setDouble(4, 1500.00);
+			st.setInt(5, 4);
 			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-				
-			}
+			int rowsAffected  = st.executeUpdate();
 			
+			System.out.println("Done " + rowsAffected);
+					
 			
 		}
 		catch(SQLException e) {
 			
 			e.printStackTrace();
-			
 		}
-		finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
+		catch(ParseException e) {
+			e.printStackTrace();
 		}
-		
-	}
 	
 
+    }
+	
 }
