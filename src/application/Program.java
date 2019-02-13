e2ee685a9afc0e8fbbbb2b178857f15c397d575b
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-02-13 13:05:33 +0000

    Transaction rollback

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,12 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import com.mysql.jdbc.SQLError;
 
 import db.DB;
+import db.DbException;
 import db.DbIntegrityException;
 
 
@@ -14,26 +17,36 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Connection conn = null;
-		
-		PreparedStatement st = null;
-		
+		Statement st = null;
 		
 		try {
 			conn = DB.getConnection();
-			st = conn.prepareStatement(
-					"DELETE FROM department where Id = ? "
-					
-					);
+			conn.setAutoCommit(false);
 			
-			st.setInt(1, 6);
+			st = conn.createStatement();
 			
-         int rowsAffected  = st.executeUpdate();
-         System.out.println("Linhas alteradas: " + rowsAffected);
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2600 WHERE departmentId =1");
+			
+		    int x =2;
+		    if(x <3) {
+		    	throw new SQLException("Fake error");
+		    }
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 2600 WHERE departmentId =2");
+			
+			conn.commit();
 		}
 		
-		catch(SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		}
+		
+		  catch(SQLException e) {
+			  try {
+				  conn.rollback();
+				  throw new DbException("Roll back not done " + e.getMessage());
+			  }catch(SQLException e1) {
+				  throw new DbException("Error trying to rollback! " + e.getMessage());
+			  }
+		  }
+		
+		
 
 		finally {
 			DB.closeStatement(st);
