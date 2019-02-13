358c7dab7a8303c1118077edece3ba8190f576fd
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-02-13 11:34:54 +0000

    void closeStatement and closeResultSet()

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,46 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
-
+		Connection conn = null;
+		
+		Statement st = null;
+		
+		ResultSet rs = null;
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement(); // Instance Statement
+			rs = st.executeQuery("select * from department"); // Query
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+				
+			}
+			
+			
+		}
+		catch(SQLException e) {
+			
+			e.printStackTrace();
+			
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
 	}
+	
 
 }
