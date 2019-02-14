ed02cd5a8fa605cbfaa863187caa0aca12901ce9
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-02-14 14:10:56 +0000

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,10 +12,16 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department dep = new Department(1, "Books");
+		
 		Seller seller  = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.00, dep);
+		
 		System.out.println(dep.toString());
 		
-		System.out.println(seller);
+		
+		
+		SellerDao sellerDao =    DaoFactory.createSellerDao(); // Program just know interface
+		
+	
     }
 	
 }
